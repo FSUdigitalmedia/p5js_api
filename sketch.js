8cfbf427973c6f9ae4af709ef450b64a9bc5a28b
Rob Duarte <me@robduarte.com>
2022-11-14 23:44:22 -0500

    mouse click version

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let got;
+let div;
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas();
+  div = createDiv();
+  div.position(50, 50);
+  div.style("font-size", "40px");
+  background(125);
+  noLoop();
 }
 
 function draw() {
-  background(125);
+  if (got != undefined) div.html(got.activity);
+  noLoop();
+}
+
+function mouseClicked() {
+  let url = 'https://www.boredapi.com/api/activity?type=relaxation';
+  loadJSON(url, "json", function(response) {
+    got = response;
+  });
+  loop();
 }
